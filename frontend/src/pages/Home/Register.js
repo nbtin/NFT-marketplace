6a134fe640360a56eaec0e13b98ffe6e006ed7cc
d2412916 <94030491+d2412916@users.noreply.github.com>
2022-12-10 22:07:17 +0700

    Add files via upload

--- frontend/src/pages/Home/Register.js
+++ frontend/src/pages/Home/Register.js
@@ -1,213 +1,135 @@
-import React, { Component } from "react";
+import React, { useState } from 'react';
 import './Register.scss'
 import 'bootstrap/dist/css/bootstrap.min.css';
-import {  toast } from 'react-toastify';
-import PhoneInput from 'react-phone-input-2'
+import { ToastContainer, toast } from 'react-toastify';
 import 'react-phone-input-2/lib/style.css'
+import google from './images/google.png';
+import facebook from './images/facebook.png';
+import eye from './images/eye.png';
+import eyeslash from './images/eyeslash.png';
+import nft from './images/nft.jpg';
 
-class Register extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            username: '',
-            email: '',
-            password: '',
-            passwordconfirm: '',
-            phone: '',
-            gender: '',
-            gendertext: '',
-            isShowPassword: false
-        };
-    }
+function Register() {
 
-    setAge(checkedValue) {
-        this.setState({
-            age: checkedValue
-        })
-    }
+    const [username, setUsername] = useState('');
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    const [passwordconfirm, setPasswordConfirm] = useState('');
+    const [isShowPassword, setIsShowPassword] = useState(false);
 
-    setGender(checkedValue) {
-        this.setState({
-            gender: checkedValue
-        })
-    }
+    // const reset = () => {
+    //     username: '';
+    //     email: '';
+    //     password: '';
+    //     passwordconfirm: '';
+    //     phone: '';
+    //     gender: '';
+    //     gendertext: '';
+    //     isShowPassword: false;
+    // }
 
-    reset() {
-        this.setState({
-            username: '',
-            email: '',
-            password: '',
-            passwordconfirm: '',
-            phone: '',
-            gender: '',
-            gendertext: '',
-            isShowPassword: false
-        })
-    }
-
-    handleOnChangeUsername = (event) => {
-        this.setState({
-            username: event.target.value
-        })
-    }
-
-    handleOnChangeEmail = (event) => {
-        this.setState({
-            email: event.target.value
-        })
-    }
-
-    handleOnChangePassword = (event) => {
-        this.setState({
-            password: event.target.value
-        })
-    }
-
-    handleOnChangePasswordConfirm = (event) => {
-        this.setState({
-            passwordconfirm: event.target.value
-        })
-    }
-
-    hanldeShowHidePassword = () => {
-        this.setState({
-            isShowPassword: !this.state.isShowPassword
-        })
-    }
-
-    hanldeRegister = (event) => {
-        if (!this.state.username ||
-            !this.state.email ||
-            !this.state.password ||
-            !this.state.passwordconfirm ||
-            !this.state.gender ||
-            !this.state.phone) {
+    const hanldeRegister = (event) => {
+        if (!username ||
+            !email ||
+            !password ||
+            !passwordconfirm) {
             toast.error(`Missing infomation!`)
             return;
         }
-        if (this.state.password.length < 6) {
+        if (password.length < 6) {
             toast.error(`Password length is at least 6 characters!`)
             return;
         }
-        if (this.state.password !== this.state.passwordconfirm) {
+        if (password !== passwordconfirm) {
             toast.error(`Passwords don't match!`)
             return;
         }
         else {
+            console.log(username, email, password, passwordconfirm)
             toast.success(`Register success`)
-            this.reset();
+            //setState(reset);
             return;
         }
     }
 
-    render() {
-        return (
-            <div className="register-background">
-                <img src="https://fsivietnam.com.vn/wp-content/uploads/2022/02/NFT-1.jpg" />
-                <div className="register-container">
-                    <div className="register-content row">
-                        <div className="col-12 text-register"> Register</div>
+    const setShowHidePassword = () => {
+        isShowPassword: setIsShowPassword(!isShowPassword);
+    }
 
-                        <div className="col-12 form-group register-input">
+    return (
+        <div className="register-background">
+            <img src={nft} />
+            <div className="register-container">
+                <div className="register-content row">
+                    <div className="col-12 text-register"> Register</div>
+                    <div className="col-12 form-group register-input">
+                        <input
+                            type="text"
+                            className="form-control"
+                            placeholder="Username"
+                            value={username}
+                            onChange={(event) => setUsername(event.target.value)} />
+                    </div>
+
+                    <div className="col-12 form-group register-input">
+                        <input
+                            type="text"
+                            className="form-control"
+                            placeholder="Email"
+                            value={email}
+                            onChange={(event) => setEmail(event.target.value)} />
+                    </div>
+
+                    <div className="col-12 form-group register-input">
+                        <div className="costum-input-pw">
                             <input
-                                type="text"
+                                type={isShowPassword ? 'text' : 'password'}
                                 className="form-control"
-                                placeholder="Username"
-                                value={this.state.username}
-                                onChange={(event) => this.handleOnChangeUsername(event)} />
+                                placeholder="Password"
+                                value={password}
+                                onChange={(event) => setPassword(event.target.value)} />
+                            <span
+                                onClick={() => { setShowHidePassword() }} >
+                                {/* <i className={isShowPassword ? 'far fa-eye' : 'far fa-eye-slash'}></i> */}
+                                <img src={isShowPassword ? eye : eyeslash} />
+                            </span>
                         </div>
+                    </div>
 
-                        <div className="col-12 form-group register-input">
-                            <input
-                                type="text"
-                                className="form-control"
-                                placeholder="Email"
-                                value={this.state.email}
-                                onChange={(event) => this.handleOnChangeEmail(event)} />
-                        </div>
+                    <div className="col-12 form-group register-input">
+                        <input
+                            type="password"
+                            className="form-control"
+                            placeholder="Confirm Password"
+                            value={passwordconfirm}
+                            onChange={(event) => setPasswordConfirm(event.target.value)} />
+                    </div>
 
-                        <div className="col-12 form-group register-input">
-                            <div className="costum-input-pw">
-                                <input
-                                    type={this.state.isShowPassword ? 'text' : 'password'}
-                                    className="form-control"
-                                    placeholder="Password"
-                                    value={this.state.password}
-                                    onChange={(event) => this.handleOnChangePassword(event)} />
-                                <span
-                                    onClick={() => { this.hanldeShowHidePassword() }} >
-                                    <i className={this.state.isShowPassword ? 'far fa-eye' : 'far fa-eye-slash'}></i>
-                                </span>
-                            </div>
-                        </div>
+                    <div className="col-12">
+                        <button className="btn-register" onClick={(event) => hanldeRegister(event)} > Register</button>
+                    </div>
 
-                        <div className="col-12 form-group register-input">
-                            <input
-                                type="password"
-                                className="form-control"
-                                placeholder="Confirm Password"
-                                value={this.state.passwordconfirm}
-                                onChange={(event) => this.handleOnChangePasswordConfirm(event)} />
-                        </div>
+                    <div className="col-12">
+                        <span className="already">Already have an account? <a target="_sefl" href="https://facebook.com">Login</a> </span>
+                    </div>
 
-                        <div className="col-12 form-group register-input">
-                            <div className="costum-input-phone" >
-                                <PhoneInput
-                                    country={'vn'}
-                                    placeholder="Phone number"
-                                    value={this.state.phone}
-                                    onChange={phone => this.setState({ phone })}
+                    <div className="col-12 text-center">
+                        <span className="text-other-register mt-3"> Or register with </span>
+                    </div>
+                    <div className="col-12 social-register">
+                        <span className="icon">
+                            <img src={google} className="google" />Continue with Google</span>
+                    </div>
+                    <div className="col-12 social-register">
+                        <span className="icon">
+                            <img src={facebook} className="facebook" />Continue with Facebook</span>
 
-                                />
-                                <i className="fas fa-phone-square fa-2x"></i>
-                            </div>
-                        </div>
-
-                        <div className="col-12 form-group register-input">
-                            <div className="col-12 form-group sex">
-                                <p className="flabel">Gender:</p>
-                                <label>
-                                    <input
-                                        type="radio"
-                                        className="gender"
-                                        checked={(this.state.gender === 'Female')}
-                                        onChange={this.setGender.bind(this, 'Female')} /> Female</label>
-                                <label>
-                                    <input
-                                        type="radio"
-                                        className="gender"
-                                        checked={(this.state.gender === 'Male')}
-                                        onChange={this.setGender.bind(this, 'Male')} /> Male</label>
-                                <label>
-                                    <input
-                                        type="radio"
-                                        className="gender"
-                                        checked={(this.state.gender === 'Other')}
-                                        onChange={this.setGender.bind(this, 'Other')} /> Other</label>
-
-                            </div>
-                        </div>
-
-                        <div className="col-12">
-                            <button className="btn-register" onClick={(event) => this.hanldeRegister(event)} > Register</button>
-                        </div>
-
-                        <div className="col-12">
-                            <span className="already">Already have an account? <a target="_sefl" href="https://facebook.com">Login</a> </span>
-                        </div>
-
-                        {/* <div className="col-12 text-center">
-                            <span className="text-other-register mt-3"> Or register with </span>
-                        </div>
-                        <div className="col-12 social-register">
-                            <i className="fab fa-google-plus-g google"></i>
-                            <i className="fab fa-facebook-f facebook"></i>
-                        </div> */}
                     </div>
                 </div>
             </div>
-        )
-    }
+        </div >
+    )
+    // }
 }
 
 
