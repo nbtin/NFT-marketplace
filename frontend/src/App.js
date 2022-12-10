68effff6abd8e0b8e5d66bf578c555d1384fefbd
taivo202 <vovantai432@gmail.com>
2022-12-10 11:27:44 +0700

    merge code

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,26 +1,17 @@
-
-import {Routes, Route} from 'react-router-dom';
+import Homepage from "./pages/Home/Homepage";
+import { Routes, Route } from 'react-router-dom';
 
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Routes>
+        <Route path="/" element={<Homepage />} default />
+        
+      </Routes>
+
+    </>
   );
 }
 
