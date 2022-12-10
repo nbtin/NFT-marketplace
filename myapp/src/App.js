0ef15057ab99e14edf1d013546ff4f9b54464c1e
taivo202 <vovantai432@gmail.com>
2022-12-10 14:25:57 +0700

    dm

---
+++ myapp/src/App.js
@@ -0,0 +1,18 @@
+import Homepage from "./pages/Home/Homepage";
+import { Routes, Route } from 'react-router-dom';
+
+import './App.css';
+
+function App() {
+  return (
+    <>
+      <Routes>
+        <Route path="/" element={<Homepage />} default />
+
+      </Routes>
+
+    </>
+  );
+}
+
+export default App;
---
+++ myapp/src/pages/Home/Homepage.jsx
@@ -0,0 +1,19 @@
+import { Button, Navbar, ListGroup, Dropdown, Carousel } from "react-bootstrap";
+import { Link } from "react-router-dom";
+import React, { useState, useEffect, useCallback } from "react";
+import NavBar from "../../components/NavBar/NavBar";
+import "./Homepage.css";
+
+function Homepage()
+{
+    return (
+    
+        <div className="background-first">
+            <NavBar />
+
+        </div>
+
+    );
+
+}
+export default Homepage;
