f66480848fb643f65a4c8b10b5b6ef106d27a876
hungt1 <nguyenvanhung.brvt.2002@gmail.com>
2022-12-16 01:07:28 +0700

    change media url

--- backend/service/settings.py
+++ backend/service/settings.py
@@ -121,8 +121,8 @@ USE_TZ = True
 STATIC_URL = "static/"
 
 # Media files
-MEDIA_ROOT = BASE_DIR / 'media'
-MEDIA_URL = '/media/'
+MEDIA_ROOT = BASE_DIR / 'media'   
+MEDIA_URL = ''
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
