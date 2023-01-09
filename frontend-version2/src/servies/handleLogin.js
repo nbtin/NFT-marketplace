c18e4ea752f87e013d55e5bec963e098482639cd
taivo202 <vovantai432@gmail.com>
2023-01-09 09:33:56 +0700

    hmm

--- frontend-version2/src/servies/handleLogin.js
+++ frontend-version2/src/servies/handleLogin.js
@@ -1,19 +1,9 @@
-<<<<<<< HEAD
-
-import {configs} from "../configs/configs"
-async function handleLoginAPI(email, password) {
-    let user ="";
-    let server = configs();
-    console.log(server+'/login');
-    await fetch(server+'/login', {
-=======
 import {configs} from "../configs/configs"
 
 async function handleLoginAPI(email, password) {
     let user =""
     let server = configs();
     await fetch(server + '/login', {
->>>>>>> 788a95ab5d0b405c0dbf211a95b076dd46890156
         method: "POST",
         header:
         {
